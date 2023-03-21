5784e59fb44bc6b8e7999b3360356640047ac028
wenkang liu <76419029+CaesarZ99@users.noreply.github.com>
2023-03-21 19:04:14 +0800

    Delete BYSJ directory

--- BYSJ/src/main/java/com/lwk/bysj/controller/PtUserController.java
+++
@@ -1,4 +0,0 @@
-package com.lwk.bysj.controller;
-
-public class PtUserController {
-}
--- BYSJ/src/main/java/com/lwk/bysj/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.lwk.bysj.service;
-
-import com.lwk.bysj.pojo.User;
-
-public interface RoleService {
-
-    void addRoleToNewUser(User u);
-
-}
